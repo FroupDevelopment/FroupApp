d2b6885528bc5631b14f4cd06ad0813d48fe1a62
RobWood8426 <robwoo8426@gmail.com>
2016-06-24 18:40:05 +0200

    Addition of swiping cards

--- Froup/www/js/app.js
+++ Froup/www/js/app.js
@@ -1,24 +1,53 @@
-// Ionic Starter App
+// Ionic Starter App, v0.9.20
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'ionic.contrib.ui.tinderCards' is found in ionic.tdcards.js
+angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+.directive('noScroll', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attr) {
+            $element.on('touchmove', function(e) {
+                e.preventDefault();
+            });
+        }
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
 })
+
+.controller('CardsCtrl', function($scope) {
+    var cardTypes = [
+    { image: 'img/pic1.png', title: 'Nike Shoe'},
+    { image: 'img/pic2.png', title: 'Nike Sport Shirts'},
+    { image: 'img/pic3.png', title: 'Addidas Shoe'},
+    { image: 'img/pic4.png', title: 'Adidas Leggings'},
+    { image: 'img/pic5.png', title: 'Kettle Bells'},
+    { image: 'img/pic6.png', title: 'Ozone Bike'},
+    ];
+
+    $scope.cards = [];
+
+    $scope.addCard = function(i) {
+        
+        var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+        newCard.id = Math.random();
+        $scope.cards.push(angular.extend({}, newCard));
+    }
+
+    for(var i = 0; i < 6; i++) $scope.addCard();
+
+    $scope.cardSwipedLeft = function(index) {
+        console.log('Left swipe');
+    }
+
+    $scope.cardSwipedRight = function(index) {
+        console.log('Right swipe');
+    }
+
+    $scope.cardDestroyed = function(index) {
+        $scope.cards.splice(index, 1);
+        console.log('Card removed');
+    }
+});
